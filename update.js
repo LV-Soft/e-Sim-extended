37bad7638a9142209c5643cbd467a1db1450a34c
LV Soft Communications <lasvarillas@gmail.com>
2015-10-04 05:16:01 -0300

    Create update.js

---
+++ update.js
@@ -0,0 +1,2 @@
+var eSimExVersion = {"version":"1.6.1","code":"0",downloadURL:"http://lv-soft.github.io/e-Sim-extended/script.user.js",updateURL:"http://lv-soft.github.io/e-Sim-extended/changelog.html"};
+XiT.checkVersion(eSimExVersion);
